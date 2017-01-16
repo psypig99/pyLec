9d1e96ae7e81d21015f1659b8780944a52bc72a8
psypig99 <psypig99>
2017-01-16 11:24:27 +0900

    if

---
+++ py3_tutorials/006_if_elif_else.py
@@ -0,0 +1,10 @@
+name = "Scarlett Johansson"
+
+if name is "Jason":
+    print("Hey there Jason")
+elif name is "Lucy":
+    print("What's up lucy")
+elif name is "Sam":
+    print("How're u doin Sam")
+else:
+    print("Please sing up for the site")
