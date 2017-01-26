b2ca9314298703df64a3f24b951e7a632b1969e4
psypig99 <psypig99@gmail.com>
2017-01-26 09:26:19 +0900

    032 update

--- py3_tutorials/basic/032_words_counters_1.py
+++ py3_tutorials/basic/032_words_counters_1.py
@@ -6,13 +6,37 @@ def start(url):
     word_list = []
     source_code = requests.get(url).text
     soup = BeautifulSoup(source_code, "lxml")
-    p_tag = soup.findAll("p", {"class":"tt-post-title"})
-
+    p_tag = soup.findAll('p', {'class':'tt-post-title'})
+    print(soup)
     for title_text in p_tag:
         content = title_text.text
         words = content.lower().split()
+        print(words)
         for each_word in words:
             print(each_word)
             word_list.append(each_word)
+
+    clean_up_list(word_list)
+
+def clean_up_list(word_list):
+    clean_word_list = []
+    for word in word_list:
+        symbols = "`~!@#$%^&*()-=_+[]\{}|;',./:\"<>?"
+        for i in range(1, len(symbols)):
+            word = word.replace(symbols[i], "")
+        if len(word) > 0:
+            # print(word)
+            clean_word_list.append(word)
+
+    create_dictionary(clean_word_list)
+
+def create_dictionary(clean_word_list):
+    word_count = {}
+    for word in clean_word_list:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+
 
 start("http://creativeworkds.tistory.com")
